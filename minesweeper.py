1c3e7e5c81f276cdb05f015bf7c2950caa94cb0d
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-09 20:23:49 +0300

    create a class

--- minesweeper.py
+++ minesweeper.py
@@ -1,5 +1,19 @@
 # we are going to use RECURSION and CLASSES for this game
 
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        # lets keep track of these parameters.
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        # let's create the board
+        # helper function:
+        self.board = self.make_new_board() # plant the bombs
+
+        # initialize a set to keep track of which locations we've uncovered
+        # we'll save(row, col) tuples into this set
+        self.dug = set()
+
 # goal of this function is to play the game
 def play(dim_size = 10, num_bombs = 10):
     # Step 1: create the board and plant bombs
