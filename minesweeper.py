9c5df8db8d734d9c548ff36868d3a1acfcfcfd26
Chebichii-Lab <chebichiiserem@gmail.com>
2024-01-10 20:43:40 +0300

    add changes

--- minesweeper.py
+++ minesweeper.py
@@ -156,7 +156,8 @@ def play(dim_size = 10, num_bombs = 10):
         board.dug = [(r,c) for r in range(board.dim_size) for c in range(board.dim_size)]  
         print(board)
 
-
+if __name__ == "__main__":  # if you have a big project and only want to run a specific file use this
+    play()
 
 
 
